11a0127b486c551e975168a500d61b4f311254d4
Moccis <erkki.koskenkorva@gmail.com>
2017-01-20 23:57:29 +0200

    Bat controls added
    Bat controls and test scene for them

---
+++ FGJ17Echo/Assets/Scripts/BatController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour {
+	[SerializeField]
+	private float moveSpeed = 15.0f;
+	[SerializeField]
+	private float maxVelocity = 100f;
+	private Rigidbody2D rb2d;
+
+	void Awake () {
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		if(rb2d.velocity.magnitude < maxVelocity) {
+			rb2d.AddForce(Vector2.right * Input.GetAxis("Horizontal") * moveSpeed);
+			rb2d.AddForce(Vector2.up * Input.GetAxis("Vertical") * moveSpeed);
+		}
+	}
+}
