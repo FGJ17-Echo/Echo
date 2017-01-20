0ead83d653c3156d74fe3fe12b3f18c285358236
Moccis <erkki.koskenkorva@gmail.com>
2017-01-21 00:47:44 +0200

    Rotation direction to orb

---
+++ FGJ17Echo/Assets/Scripts/DirectionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionController : MonoBehaviour {
+
+	void Update () {
+		var euler = transform.localEulerAngles;
+		euler.z = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * (180 / Mathf.PI);
+		transform.localEulerAngles = euler;
+	}
+}
