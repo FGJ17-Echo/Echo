2abd4a4a914842d1f588ce86369178d8107e3f63
Moccis <erkki.koskenkorva@gmail.com>
2017-01-21 15:49:07 +0200

    Rough sketch to GameManager

---
+++ FGJ17Echo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public enum gameState
+	{
+		DEFAULT,
+		GAMESTART,
+		PLAYING,
+		GAMEOVER,
+		MENU
+	};
+	public gameState gs = gameState.DEFAULT;
+
+	void Update () {
+		switch (gs)
+		{
+		case gameState.GAMESTART:
+			Debug.Log("Load all the assets");
+			break;
+		case gameState.PLAYING:
+			Debug.Log("We are playing now");
+			break;
+		case gameState.GAMEOVER:
+			Debug.Log("Game over");
+			break;
+		case gameState.MENU:
+			Debug.Log("Menu");
+			break;
+		default:
+			Debug.Log("ERROR: Unknown game state: " + gs);
+			break;
+		}
+	}
+}
