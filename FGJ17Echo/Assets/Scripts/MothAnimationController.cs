7c31a425a45adbde01702390ee2aab042e10afd0
Timo Moisio <timohmoisio@gmail.com>
2017-01-21 22:27:01 +0200

    Add changing moth animation speed based on movement

--- FGJ17Echo/Assets/Scripts/MothAnimationController.cs
+++ FGJ17Echo/Assets/Scripts/MothAnimationController.cs
@@ -7,6 +7,9 @@ public class MothAnimationController : MonoBehaviour
     [SerializeField]
 	private Rigidbody2D _rb2d;
 
+    [SerializeField]
+    private Animator _animator;
+
 	void Update () {
 		var direction = new Vector3(_rb2d.velocity.x, _rb2d.velocity.y, 0);
 
@@ -14,5 +17,8 @@ public class MothAnimationController : MonoBehaviour
         if (_rb2d.velocity.x < 0) angle = 360 - angle;
         angle = -angle;
 		transform.eulerAngles = new Vector3(0, 0, angle);
-	}
+
+        _animator.speed = Mathf.Clamp(direction.magnitude * 4, 0.5f, 2);
+
+    }
 }
