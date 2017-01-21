008726d340a9af430b3e636cdabfa26eba1fd68a
Timo Moisio <timohmoisio@gmail.com>
2017-01-21 20:20:00 +0200

    Change moth animation direction

--- FGJ17Echo/Assets/Scripts/MothAnimationController.cs
+++ FGJ17Echo/Assets/Scripts/MothAnimationController.cs
@@ -1,21 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MothAnimationController : MonoBehaviour {
-
+public class MothAnimationController : MonoBehaviour
+{
+    [SerializeField]
 	private Rigidbody2D _rb2d;
-
-	void Awake () 
-	{
-		_rb2d = transform.parent.GetComponent<Rigidbody2D>();
-	}
 
 	void Update () {
 		var direction = new Vector3(_rb2d.velocity.x, _rb2d.velocity.y, 0);
 
 		var angle = Vector3.Angle(direction, Vector3.up);
-		if (_rb2d.velocity.x < 0) angle = 360 - angle;
+        if (_rb2d.velocity.x < 0) angle = 360 - angle;
+        angle = -angle;
 		transform.eulerAngles = new Vector3(0, 0, angle);
 	}
 }
